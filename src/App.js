87046e94f304667f2588162f485454847e6b3c65
JKyEun <j56237@naver.com>
2023-03-25 22:24:49 +0900

    test2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './routes/Home';
 import Detail from './routes/Detail';
 
 function App() {
   return (
-    <div>
-      <div>안녕하세요 이거 뜨나요?</div>
-      <Router basename={process.env.PUBLIC_URL}>
-        <Routes>
-          <Route path='/' element={<Home />}></Route>
-          <Route path='/movie/:id' element={<Detail />}></Route>
-        </Routes>
-      </Router>
-    </div>
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/movie/:id' element={<Detail />}></Route>
+    </Routes>
   );
 }
 
